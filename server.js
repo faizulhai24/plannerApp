c1204fe99f603f574a38581311d53f4d8a3e8e4c
faizulhai24 <tarun.chaudhary@livspace.com>
2015-10-16 10:10:30 +0530

    changed name in server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var express = require('express'),
 app
 	.use(express.static('./public'))
 	.get('*', function(req,res){
-		res.sendFile(__dirname + '/public/main.html');
+		res.sendFile(__dirname + '/public/index.html');
 	})
 	.listen(3000); 
